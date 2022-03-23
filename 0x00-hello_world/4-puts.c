183db2d4d322a3c0365239ba2f8144f65cae010e
BBWorksB <bonnybaraka001@gmail.com>
2022-03-23 00:44:27 -0700

    uSE PUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+/**
+*main -entry point
+*Description: using the main function
+*it will print "Programming is like building a multilingual puzzle
+*Return:0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
