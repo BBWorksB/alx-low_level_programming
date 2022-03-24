c5b3a6938a180b95859a1fb39d94812e9a53dd2f
BBWorksB <bonnybaraka001@gmail.com>
2022-03-23 22:20:00 -0700

    Lets count

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main will print all the numbers
+* 
+* between 0 and 10
+* Description: using the main function
+* the program prints 0,1,2,3....10
+* Return:0 
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
+}
